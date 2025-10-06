a7baf2a80329cdf03ef26a57b338ffdd30be4151
Marco Sanavia <sanaistheway@hotmail.it>
2025-10-06 18:17:50 +0100

    feature: Github IAC

--- modules/github/locals.tf
+++ modules/github/locals.tf
@@ -1,13 +1,13 @@
 locals {
   dependabot_enabled = {
-    for k, v in var.repositories : k => v if v.dependabot_enable != false
+    for k, v in var.repositories : k => v if v.enable_dependabot == true
   }
   public_repositories = {
     for k, v in var.repositories : k => v if v.visibility == "public"
   }
   repository_collaborators = flatten([
     for repo_name, repo_config in var.repositories : [
-      for username, permission in lookup(repo_config, "collaborators", {}) : {
+      for username, permission in coalesce(lookup(repo_config, "collaborators", null), {}) : {
         repo_name  = repo_name
         username   = username
         permission = permission
